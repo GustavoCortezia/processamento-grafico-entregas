7963e7d2e622ee16e93281390aaae0c307bf77e8
Gustavo Cortezia <gustavo.cortezia@sap.com>
2025-09-03 19:52:41 -0300

    initial-commit

---
+++ PG2025-2/setPath.bat
@@ -0,0 +1,6 @@
+set PATH=%PATH%;C:\msys64\ucrt64\bin;
+@REM C:\Users\rossanaqueiroz\Documents\cmake-4.0.2-windows-x86_64\bin;
+C:\Users\I762684\Downloads\cmake-4.1.0-windows-x86_64.msi;
+@REM C:\Users\rossanaqueiroz\Documents\PortableGit\bin
+C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Git
+code .
